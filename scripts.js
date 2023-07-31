ca0426db71808e4f79b5624060b3067c3bd5df2b
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-07-31 14:17:05 +0200

    update IWA1

--- scripts.js
+++ scripts.js
@@ -1,40 +1,38 @@
 //global const
-const MAX_NUMBER=15
-const MIN_NUMBER= -5
-const STEP_AMOUNT= 5;
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
+const STEP_AMOUNT = 5;
 
-const number = document.querySelector ('[data-key="number"]')
-const subtract =document.querySelector ('[data-key="subtract"]')
-const add =document.querySelector ('[data-key="add"]')
+const number = document.querySelector('[data-key="number"]');
+const subtract = document.querySelector('[data-key="subtract"]');
+const add = document.querySelector('[data-key="add"]');
 
 //Say this second
-const subtractHandler =() => {
-    newValue=parseInt(number.value)-STEP_AMOUNT //changing string to a number
-    number.value=newValue 
+const subtractHandler = () => {
+  newValue = parseInt(number.value) - STEP_AMOUNT; //changing string to a number
+  number.value = newValue;
 
-    if (add.disabled ===true ){
-        add.disabled=false
-    }
+  if (add.disabled === true) {
+    add.disabled = false;
+  }
 
-    if (newValue <= MIN_NUMBER) {
-        subtract.disabled=true
-    
-}
-}
-const addHandler =() =>{
-    newValue=parseInt(number.value) +STEP_AMOUNT
-    number.value= newValue //first code
+  if (newValue <= MIN_NUMBER) {
+    subtract.disabled = true;
+  }
+};
+const addHandler = () => {
+  newValue = parseInt(number.value) + STEP_AMOUNT;
+  number.value = newValue; //first code
 
- if ( subtract.disabled ===true) {
-      subtract.disabled=false
- }
+  if (subtract.disabled === true) {
+    subtract.disabled = false;
+  }
 
+  if (newValue >= MAX_NUMBER) {
+    add.disabled = true;
+  }
+};
 
-    if (newValue >= MAX_NUMBER) {
-        add.disabled=true
-    }
-}
-
-// say this first 
-subtract.addEventListener('click',subtractHandler)
-add.addEventListener('click',addHandler)
+// say this first
+subtract.addEventListener("click", subtractHandler);
+add.addEventListener("click", addHandler);
